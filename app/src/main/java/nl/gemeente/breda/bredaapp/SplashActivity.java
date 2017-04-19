728cf22aa3326cdf3ebe07d03bfe6adc7a91f3af
Ilthy <giedevangils@gmail.com>
2017-04-19 14:25:16 +0200

    Aanpassen kleurenschema & langere laadtijd

--- app/src/main/java/nl/gemeente/breda/bredaapp/SplashActivity.java
+++ app/src/main/java/nl/gemeente/breda/bredaapp/SplashActivity.java
@@ -26,7 +26,7 @@ public class SplashActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_splash);
 
-        new CountDownTimer(2000, 1000) {
+        new CountDownTimer(2543, 1000) {
 
             @Override
             public void onTick(long millisUntilFinished) {
