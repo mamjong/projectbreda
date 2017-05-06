581de53294b65f7b1cc94c83a5f3cc344c30fd11
Thomas Mout <stmout@avans.nl>
2017-05-06 14:32:51 +0200

    Maps toevoegen

--- app/src/main/java/nl/gemeente/breda/bredaapp/MainActivity.java
+++ app/src/main/java/nl/gemeente/breda/bredaapp/MainActivity.java
@@ -3,11 +3,29 @@ package nl.gemeente.breda.bredaapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-	
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+		mapFragment.getMapAsync(this);
+	}
+
+	@Override
+	public void onMapReady(GoogleMap googleMap) {
+		LatLng breda = new LatLng(51.5831, 4.777);
+		float zoom = 12;
+		googleMap.addMarker(new MarkerOptions().position(breda).title("Marker in Breda"));
+		googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(breda, zoom));
 	}
 }
