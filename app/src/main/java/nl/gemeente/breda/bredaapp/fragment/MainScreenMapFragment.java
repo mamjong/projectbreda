c3801b92b1d77f23a75732c005c767df44fb03c0
Thimo Laptop <tkoolen1@student.avans.nl>
2017-05-08 11:14:43 +0200

    Fixed maps!

--- app/src/main/java/nl/gemeente/breda/bredaapp/fragment/MainScreenMapFragment.java
+++ app/src/main/java/nl/gemeente/breda/bredaapp/fragment/MainScreenMapFragment.java
@@ -2,6 +2,9 @@ package nl.gemeente.breda.bredaapp.fragment;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,15 +33,24 @@ public class MainScreenMapFragment extends Fragment implements OnMapReadyCallbac
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		final View rootView = inflater.inflate(R.layout.fragment_map_view, container, false);
 		
-		//SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager().findFragmentById(R.id.fragmentMapView_FR_map);
-		//mapFragment.getMapAsync(this);
+		SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.fragmentMapView_FL_mapLayout);
+		if (mapFragment == null) {
+			FragmentManager fragmentManager = getFragmentManager();
+			FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+			mapFragment = SupportMapFragment.newInstance();
+			fragmentTransaction.replace(R.id.fragmentMapView_FL_mapLayout, mapFragment).commit();
+		}
+		
+		if (mapFragment != null) {
+			mapFragment.getMapAsync(this);
+		}
 		
 		return rootView;
 	}
 	
 	@Override
 	public void onMapReady(GoogleMap googleMap) {
-		LatLng breda = new LatLng(51.5831, 4.777);
+		LatLng breda = new LatLng(51.5853953, 4.7929303);
 		float zoom = 12;
 		googleMap.addMarker(new MarkerOptions().position(breda).title("Marker in Breda"));
 		googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(breda, zoom));
