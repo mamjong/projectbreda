4e2b401d7874e90ee32e9e3b7d11a599bff71f34
Thomas <thomas_641991@hotmail.com>
2017-05-08 11:08:10 +0200

    Added functionality for continue button

--- app/src/main/java/nl/gemeente/breda/bredaapp/TermsAndConditionsActivity.java
+++ app/src/main/java/nl/gemeente/breda/bredaapp/TermsAndConditionsActivity.java
@@ -2,6 +2,8 @@ package nl.gemeente.breda.bredaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TermsAndConditionsActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class TermsAndConditionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terms_and_conditions);
+
+        Button continueButton = (Button)findViewById(R.id.activityTermsAndConditions_bt_continue);
+
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TermsAndConditionsActivity.super.onBackPressed();
+            }
+        });
     }
 }
