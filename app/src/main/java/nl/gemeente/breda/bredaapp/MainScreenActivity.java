61b117777f25ef7d5fb24e14954b82e764c2bee5
Thomas Mout <stmout@avans.nl>
2017-05-09 09:11:44 +0200

    ApiConnector Homescreen and domain class Report

--- app/src/main/java/nl/gemeente/breda/bredaapp/MainScreenActivity.java
+++ app/src/main/java/nl/gemeente/breda/bredaapp/MainScreenActivity.java
@@ -5,10 +5,17 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 import nl.gemeente.breda.bredaapp.adapter.MainScreenSectionsPagerAdapter;
+import nl.gemeente.breda.bredaapp.api.ApiHomeScreen;
+import nl.gemeente.breda.bredaapp.domain.Report;
+import nl.gemeente.breda.bredaapp.testing.LocationActivity;
 
-public class MainScreenActivity extends AppCompatActivity {
+public class MainScreenActivity extends AppCompatActivity implements ApiHomeScreen.Listener {
 	
 	//================================================================================
 	// Properties
@@ -36,5 +43,18 @@ public class MainScreenActivity extends AppCompatActivity {
 		
 		TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
 		tabLayout.setupWithViewPager(viewPager);
+
+		getReports();
+	}
+
+	public void getReports() {
+		ApiHomeScreen apiHomeScreen = new ApiHomeScreen(this);
+		String[] urls = new String[] {"https://asiointi.hel.fi/palautews/rest/v1/requests.json?status=open&service_code=198&lat=60.1892477&long=24.9707467&radius=500"};
+		apiHomeScreen.execute(urls);
+	}
+
+	@Override
+	public void onReportAvailable(Report report) {
+		Log.i("REPORT", report.getDescription());
 	}
 }
