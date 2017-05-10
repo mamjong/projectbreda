f8d92c88bf541472de37a92839a83de21b5d2e35
Mark <mamjong@avans.nl>
2017-05-10 12:42:17 +0200

    Navigatie fix.

--- app/src/main/java/nl/gemeente/breda/bredaapp/AddEmailActivity.java
+++ app/src/main/java/nl/gemeente/breda/bredaapp/AddEmailActivity.java
@@ -1,14 +1,28 @@
 package nl.gemeente.breda.bredaapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddEmailActivity extends AppCompatActivity {
+
+    private Button emailConfirmBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_add_email);
+
+        emailConfirmBtn = (Button) findViewById(R.id.AddEmailActivity_bt_emailConfirmButton);
+        emailConfirmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainScreenActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
