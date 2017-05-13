31e627f684dce0ee3295cba4d560a79600cd2d73
Thimo Laptop <tkoolen1@student.avans.nl>
2017-05-13 11:36:48 +0200

    Added animated check mark, pulled develop.

--- app/src/main/java/nl/gemeente/breda/bredaapp/ReportReceivedActivity.java
+++ app/src/main/java/nl/gemeente/breda/bredaapp/ReportReceivedActivity.java
@@ -5,12 +5,17 @@ import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import cdflynn.android.library.checkview.CheckView;
+
 public class ReportReceivedActivity extends AppCompatActivity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_report_received);
+		
+		CheckView checkView = (CheckView) findViewById(R.id.ReportReceivedActivity_cv_checkview);
+		checkView.check();
 		
 		new CountDownTimer(2543, 1000) {
 			
