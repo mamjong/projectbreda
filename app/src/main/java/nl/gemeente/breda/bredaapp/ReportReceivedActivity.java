0073e8a393acc9659f4f0fc9ad32ac24a75a3ff0
Thimo Laptop <tkoolen1@student.avans.nl>
2017-05-15 12:26:58 +0200

    Delayed checkmark and textview on report received

--- app/src/main/java/nl/gemeente/breda/bredaapp/ReportReceivedActivity.java
+++ app/src/main/java/nl/gemeente/breda/bredaapp/ReportReceivedActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import cdflynn.android.library.checkview.CheckView;
 
@@ -14,8 +15,24 @@ public class ReportReceivedActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_report_received);
 		
-		CheckView checkView = (CheckView) findViewById(R.id.ReportReceivedActivity_cv_checkview);
-		checkView.check();
+		final CheckView checkView = (CheckView) findViewById(R.id.ReportReceivedActivity_cv_checkview);
+		final TextView textView = (TextView) findViewById(R.id.ReportReceivedActivity_tv_ReceivedText); 
+		//checkView.check();
+		
+		new CountDownTimer(1000, 1000) {
+			
+			@Override
+			public void onTick(long millisUntilFinished) {
+				
+			}
+			
+			@Override
+			public void onFinish() {
+				checkView.check();
+				textView.setText(getResources().getString(R.string.ReportReceivedActivity_tv_ReceivedText));
+			}
+			
+		}.start();
 		
 		new CountDownTimer(2543, 1000) {
 			
