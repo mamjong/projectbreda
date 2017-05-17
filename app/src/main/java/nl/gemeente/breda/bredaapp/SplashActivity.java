f8c34c7cba47c96a3f4599d5e24900ef95d534ab
Thomas Mout <stmout@avans.nl>
2017-05-17 11:39:57 +0200

    implement eastereggs on splashscreen

--- app/src/main/java/nl/gemeente/breda/bredaapp/SplashActivity.java
+++ app/src/main/java/nl/gemeente/breda/bredaapp/SplashActivity.java
@@ -8,15 +8,28 @@ package nl.gemeente.breda.bredaapp;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import java.util.Random;
+
+import nl.gemeente.breda.bredaapp.eastereggs.TestEasterEgg;
+import nl.gemeente.breda.bredaapp.fragment.MainScreenListFragment;
+import nl.gemeente.breda.bredaapp.fragment.MainScreenMapFragment;
 
 
 public class SplashActivity extends AppCompatActivity {
 
+	private int i;
+	private CountDownTimer timer;
+	
     //================================================================================
     // Mutators
     //================================================================================
@@ -48,8 +61,44 @@ public class SplashActivity extends AppCompatActivity {
 
         ProgressBar pb = (ProgressBar) findViewById(R.id.activitySplashScreen_pb_loader);
         pb.getIndeterminateDrawable().setColorFilter(Color.parseColor("#d91d49"), android.graphics.PorterDuff.Mode.SRC_ATOP);
+	
+	    i = 1;
+	    ImageView logo = (ImageView) findViewById(R.id.activitySplashScreen_iv_logo);
+	    logo.setOnClickListener(new View.OnClickListener() {
+		    @Override
+		    public void onClick(View v) {
+				if(i < 5){
+					i++;
+				}
+				else if(i == 5){
+					timer.cancel();
 
-        new CountDownTimer(2543, 1000) {
+					Random r = new Random();
+					int rand = r.nextInt(10) + 1;
+					Log.i("RANDOM", "" + rand);
+					
+					switch (rand) {
+//						case 1:
+//						case 2:
+//						case 3:
+//						case 4:
+//						case 5:
+//						case 6:
+//						case 7:
+//						case 8:
+//						case 9:
+//						case 10:
+						
+						default:
+							Intent easteregg = new Intent(getApplicationContext(), TestEasterEgg.class);
+							startActivity(easteregg);
+					}
+				}
+			    
+		    }
+	    });
+	    
+        timer = new CountDownTimer(2543, 1000) {
 
             @Override
             public void onTick(long millisUntilFinished) {
@@ -73,6 +122,8 @@ public class SplashActivity extends AppCompatActivity {
                 }
                 finish();
             }
-        }.start();
+        };
+	    
+        timer.start();
     }
 }
---
+++ app/src/main/java/nl/gemeente/breda/bredaapp/eastereggs/TestEasterEgg.java
@@ -0,0 +1,15 @@
+package nl.gemeente.breda.bredaapp.eastereggs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import nl.gemeente.breda.bredaapp.R;
+
+public class TestEasterEgg extends AppCompatActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_test_easter_egg);
+	}
+}
