89c127da421835172868752646ef9b157abf3239
Thomas <thomas_641991@hotmail.com>
2017-05-19 14:10:27 +0200

    Added zooming for fullscreen image detailed report

--- app/src/main/java/nl/gemeente/breda/bredaapp/DetailedReportActivityImage.java
+++ app/src/main/java/nl/gemeente/breda/bredaapp/DetailedReportActivityImage.java
@@ -1,26 +1,36 @@
 package nl.gemeente.breda.bredaapp;
 
 import android.content.res.Configuration;
+import android.graphics.Matrix;
+import android.graphics.PointF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.FloatMath;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
+import com.github.chrisbanes.photoview.PhotoViewAttacher;
+
 import nl.gemeente.breda.bredaapp.api.ImageLoader;
 import nl.gemeente.breda.bredaapp.domain.Report;
 
+import static android.R.attr.spacing;
 import static nl.gemeente.breda.bredaapp.fragment.MainScreenListFragment.EXTRA_REPORT;
 
-public class DetailedReportActivityImage extends AppCompatActivity {
+public class DetailedReportActivityImage extends AppCompatActivity /*implements View.OnTouchListener*/{
 	private ImageView mediaUrl;
+	
 		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_detailed_report_fullscreen_image);
 		
+		//remove actionbar & statusbar when in landscape mode
 		if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
 			getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE
 															|View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
@@ -33,6 +43,8 @@ public class DetailedReportActivityImage extends AppCompatActivity {
 		}
 	
 		mediaUrl = (ImageView) findViewById(R.id.DetailedReportActivityFullscreenImage_IV_Image);
+		PhotoViewAttacher photoView = new PhotoViewAttacher(mediaUrl);
+		photoView.update();
 		
 		Bundle extras = getIntent().getExtras();
 		final Report r = (Report) extras.getSerializable(EXTRA_REPORT);
