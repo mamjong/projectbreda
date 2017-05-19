b2e3a7dfe0694af932840e32fce8588eab7f027d
Thomas <thomas_641991@hotmail.com>
2017-05-19 11:27:21 +0200

    Removed actionbar on fullscreen detailed image

--- app/src/main/java/nl/gemeente/breda/bredaapp/DetailedReportActivityImage.java
+++ app/src/main/java/nl/gemeente/breda/bredaapp/DetailedReportActivityImage.java
@@ -1,7 +1,11 @@
 package nl.gemeente.breda.bredaapp;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import nl.gemeente.breda.bredaapp.api.ImageLoader;
@@ -17,6 +21,16 @@ public class DetailedReportActivityImage extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_detailed_report_fullscreen_image);
 		
+		if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+			getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE
+															|View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+															|View.SYSTEM_UI_FLAG_FULLSCREEN
+															|View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+															|View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+															|View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+		} else {
+			getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		}
 	
 		mediaUrl = (ImageView) findViewById(R.id.DetailedReportActivityFullscreenImage_IV_Image);
 		
