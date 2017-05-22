e4a661858dedd89c32e3a7785d2fc1803b812f18
Mark <mamjong@avans.nl>
2017-05-22 14:31:31 +0200

    Email aanpassen.

--- app/src/main/java/nl/gemeente/breda/bredaapp/UserSettingsActivity.java
+++ app/src/main/java/nl/gemeente/breda/bredaapp/UserSettingsActivity.java
@@ -2,6 +2,11 @@ package nl.gemeente.breda.bredaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import nl.gemeente.breda.bredaapp.domain.User;
@@ -9,6 +14,7 @@ import nl.gemeente.breda.bredaapp.domain.User;
 public class UserSettingsActivity extends AppCompatActivity{
 	
 	private User user;
+	private Button changeInfo;
 	private EditText currentEmail;
 	
 	@Override
@@ -19,8 +25,41 @@ public class UserSettingsActivity extends AppCompatActivity{
 		DatabaseHandler dbh = new DatabaseHandler(getApplicationContext(), null, null, 1);
 		
 		currentEmail = (EditText) findViewById(R.id.UserSettingsActivity_et_currentEmail);
-		user = dbh.getUser();
+		currentEmail.setEnabled(false);
 		
+		user = dbh.getUser();
 		currentEmail.setText(user.getMailAccount());
+		
+		changeInfo = (Button) findViewById(R.id.UserSettingsActivity_bt_changeInfoBtn);
+		changeInfo.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				
+				changeInfo.setVisibility(View.GONE);
+				currentEmail.setEnabled(true);
+			}
+		});
+		
+		currentEmail.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+				
+			}
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				if (s.toString().trim().length() == 0) {
+					//button.setEnabled(false);
+				} else if (Patterns.EMAIL_ADDRESS.matcher(s).matches()){
+					//button.setEnabled(true);
+				}
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				
+			}
+		});
+		
 	}
 }
