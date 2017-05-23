4098692314da4610137344c86718fd0d5e97fb4f
Mark <mamjong@avans.nl>
2017-05-23 15:17:49 +0200

    Radius Settings in progress.

--- app/src/main/java/nl/gemeente/breda/bredaapp/UserSettingsActivity.java
+++ app/src/main/java/nl/gemeente/breda/bredaapp/UserSettingsActivity.java
@@ -1,6 +1,7 @@
 package nl.gemeente.breda.bredaapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -10,7 +11,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import nl.gemeente.breda.bredaapp.domain.User;
 
@@ -19,21 +22,53 @@ public class UserSettingsActivity extends AppCompatActivity{
 	private User user;
 	private EditText currentEmail;
 	private Switch changeSettings;
-	private CharSequence currentEmailSequence;
+	private int reportRadius;
+	private TextView reportRadiusView;
+//	public static final String PREFS_NAME = "PrefsFile";
+	private SeekBar changeRadius;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_user_settings);
 		
+//		reportRadius = 1000;
+//
+//		SharedPreferences.Editor editor = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
+//		editor.putInt("ReportRadius", reportRadius);
+//		editor.commit();
+		
 		final DatabaseHandler dbh = new DatabaseHandler(getApplicationContext(), null, null, 1);
 		currentEmail = (EditText) findViewById(R.id.UserSettingsActivity_et_currentEmail);
 		changeSettings = (Switch) findViewById(R.id.UserSettingsActivity_sw_ChangeSettings);
+		changeRadius = (SeekBar) findViewById(R.id.UserSettingsActivity_sb_ChangeRadius);
+		reportRadiusView = (TextView) findViewById(R.id.UserSettingsActivity_tv_currentRadius);
+		reportRadiusView.setText("100 meters");
 		
 		currentEmail.setEnabled(false);
 		
 		user = dbh.getUser();
 		currentEmail.setText(user.getMailAccount());
+		
+		changeRadius.setProgress(0);
+		changeRadius.setMax(19);
+		changeRadius.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+				reportRadius = progress * 100 + 100;
+				reportRadiusView.setText(reportRadius + " meters");
+			}
+			
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				
+			}
+			
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				
+			}
+		});
 		
 		changeSettings.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
