c37a6ebb92011d9e690e951bfd67852d5f3c7fb9
Mark <mamjong@avans.nl>
2017-05-24 15:09:49 +0200

    Spinner Added

--- app/src/main/java/nl/gemeente/breda/bredaapp/UserSettingsActivity.java
+++ app/src/main/java/nl/gemeente/breda/bredaapp/UserSettingsActivity.java
@@ -8,10 +8,12 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -22,10 +24,12 @@ public class UserSettingsActivity extends AppCompatActivity{
 	private User user;
 	private EditText currentEmail;
 	private Switch changeSettings;
+	private Spinner themeSpinner;
 	private int reportRadius, seekBarPos;
 	private TextView reportRadiusView;
 	public static final String PREFS_NAME = "PrefsFile";
 	private SeekBar changeRadius;
+	private String[] themeSpinnerEntries;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +40,20 @@ public class UserSettingsActivity extends AppCompatActivity{
 		reportRadius = preferences.getInt("ReportRadius", 500);
 		seekBarPos = preferences.getInt("SeekBarPos", 4);
 		
+		
+		
 		final DatabaseHandler dbh = new DatabaseHandler(getApplicationContext(), null, null, 1);
 		currentEmail = (EditText) findViewById(R.id.UserSettingsActivity_et_currentEmail);
 		changeSettings = (Switch) findViewById(R.id.UserSettingsActivity_sw_ChangeSettings);
 		changeRadius = (SeekBar) findViewById(R.id.UserSettingsActivity_sb_ChangeRadius);
+		themeSpinner = (Spinner) findViewById(R.id.UserSettingsActivity_sp_ChangeTheme);
 		reportRadiusView = (TextView) findViewById(R.id.UserSettingsActivity_tv_currentRadius);
 		reportRadiusView.setText(reportRadius + " meters");
+		
+		this.themeSpinnerEntries = getResources().getStringArray(R.array.themeSpinner);
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_layout_custom_row, themeSpinnerEntries);
+		themeSpinner.setAdapter(adapter);
 		
 		currentEmail.setEnabled(false);
 		
